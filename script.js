6d9666075b5983a696984d9a178481b4fadf87e5
thetamannakhurana <115238314+thetamannakhurana@users.noreply.github.com>
2025-01-31 23:34:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Countdown to 10 PM
+// Countdown to 9 PM
 const targetTime = new Date();
-targetTime.setHours(22, 0, 0, 0); // Countdown to 10 PM
+targetTime.setHours(21, 0, 0, 0); // Countdown to 9 PM
 
 function updateCountdown() {
     const now = new Date();
